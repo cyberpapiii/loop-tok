b76c12ea1f810ed22cf0c028ce3288063ea10738
cyberpapi <robdezendorf@gmail.com>
2023-05-31 23:06:45 -0400

    Update chat.tsx

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -8,7 +8,7 @@ const Chat: FC<{chat: TiktokChat, pinned?: boolean}> = ({
 }) => {
   return <div className={styles.chat + ` ${pinned ? styles.chatPinned : ''}`}>
     <div className={styles.profile}>
-     <img src={chat.profilePictureUrl ? chat.profilePictureUrl.replace(/^https:/, 'http:') : 'default-image-url'} height={28} width={28} />
+     <img src={chat.profilePictureUrl ? chat.profilePictureUrl.replace(/^https:/, 'http:') : 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'} height={28} width={28} />
 
       <div className={styles.username}>{pinned ? '@' : ''}{chat.uniqueId}{pinned ? ':' : ''}</div>
     </div>
